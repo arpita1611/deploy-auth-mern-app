427c4debb2bbe5ea895a4b3a97c474cefd31db6f
arpita1611 <arpitasahu2005@gmail.com>
2025-07-16 02:14:04 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -21,7 +21,7 @@ function Home() {
   }
   const fetchProducts = async () => {
     try{
-      const url = "http://localhost:8080/products";
+      const url = "https://deploy-auth-mern-app-api-nine.vercel.app/products";
       const headers = {
         headers : {
           'Authorization' : localStorage.getItem('token')
